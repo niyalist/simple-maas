a399fbfd54041ddf34d027630c26030b85bc7c01
Masaki Ito <mito@iis.u-tokyo.ac.jp>
2019-06-21 23:06:24 +0900

    名称gtfs-okayamaをsimple-maasに変更

--- db.js
+++ db.js
@@ -3,6 +3,6 @@ var options = {
 };
 
 var pgp = require("pg-promise")(options);
-var db = pgp("postgres://niya@localhost:5432/gtfs-okayama");
+var db = pgp("postgres://niya@localhost:5432/gtfs-saga");
  
 module.exports = db;
